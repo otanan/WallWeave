862146e140d2d0e28d4560f241db620dfa554e2a
Jonathan Delgado <jonathandelgado0327@gmail.com>
2023-09-18 23:30:39 -0700

    push of proof of concept

---
+++ wallweave/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Top-level init file to make package available for import.
+
+Handles importing and setting project-wide fields.
+
+**Author: Jonathan Delgado**
+
+"""
+#------------- Fields -------------#
+__author__ = 'Jonathan Delgado'
+__version__ = '0.0.0.0'
+
+#------------- Imports -------------#
+from .wallweave import *
